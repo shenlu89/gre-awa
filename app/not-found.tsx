daec45ae29f5cf92f28f09ad197c50e33f8552e4
shenlu89 <shenlu89dev@gmail.com>
2025-12-11 18:54:03 +0800

    style: replace logo with text title for consistency

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <section className='flex flex-col h-full items-center mt-16 space-y-4'>
-      <Image src={'/logo.svg'} alt={''} width={80} height={80} />
+      <h1 className='font-extrabold text-5xl tracking-tight text-green-500'>GRE AWA</h1>
       <h1 className='font-extrabold text-5xl tracking-tight'>404</h1>
       <span className='text-gray-600 dark:text-gray-400'>
         Page Not Found. Go Back to{' '}
